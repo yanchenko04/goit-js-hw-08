fc74567258fc55c6106ac0d6387b64a2e3e5b3d4
yanchenko04 <yanchenko0405@gmail.com>
2022-09-19 22:28:42 +0300

    223

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+
 // Add imports above this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+console.log(galleryItems);
+
+const imgContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+imgContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" 
+            href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" 
+        />
+        </a>
+`;
+    }).join(''); 
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    
+    
+});
 console.log(galleryItems);
