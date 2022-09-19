9681f76ee2309ce730521e8bf9466c225448533b
yanchenko04 <yanchenko0405@gmail.com>
2022-09-19 23:46:40 +0300

    444

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form  textarea'),
+    input: document.querySelector('.feedback-form input')
+}
+populateTextarea()
+
+refs.form.addEventListener('submit', onFormSumnit)
+refs.textarea.addEventListener('input', onTextareaInput)
+
+function onTextareaInput(e){
+    const message = e.target.value
+    localStorage.setItem('feedback-msg', message)
+    
+
+
+}
+function onFormSumnit(e){
+    e.preventDefault()
+
+    e.currentTarget.reset()
+    localStorage.removeItem('feedback-msg')
+
+}
+function populateTextarea(){
+    const savedMessage = localStorage.getItem('feedback-msg')
+    if(savedMessage){
+        refs.textarea.value = savedMessage
+    }
+}
